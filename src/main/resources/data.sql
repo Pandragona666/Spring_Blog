be9c3d893256838192ace8094d4f7019293481d6
Pandragona <asiuspospolitus@gmail.com>
2019-07-13 09:59:51 +0200

    correcting categories for creaig and updating posts changed data base on remotemysql.com

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO role values (1, "role_user");
+INSERT INTO role values (2, "role_admin");
+
+-- INSERT INTO user values (1, 1, 'user', '$2y$12$SRyS68pPo1yMY5wdeL2fv.CRif0VlvgcJSe4GG7Fb6W3GlcnOFuDO', '2019-07-13');
+-- INSERT INTO user values (2, 1, 'admin', '$2y$12$SWKyobJpPu00mDZIJK5QD.nQioGx891Z/JleV/iYdvTS3qWglCGla', '2019-07-13');
+--
+-- INSERT INTO user_role VALUES (1, 1);
+-- INSERT INTO user_role VALUES (2, 1);
+-- INSERT INTO user_role VALUES (2, 2);
